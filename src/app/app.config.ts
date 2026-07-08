df6785d956bfcae956d298043cc438766216274c
ari-dv <rosalesariana102@gmail.com>
2026-07-08 08:24:47 -0500

    on 5

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,23 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+// 1. Añadimos withInterceptorsFromDi y HTTP_INTERCEPTORS
+import { provideHttpClient, withFetch, withInterceptorsFromDi, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeng/themes/aura'; 
 import { routes } from './app.routes';
+import { ApiKeyInterceptor } from './services/api-key.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient(withFetch()),
+    
+    // 3. Modificamos el HttpClient para que acepte interceptores de clase
+    provideHttpClient(withFetch(), withInterceptorsFromDi()),
+    
+    // 4. Registramos tu interceptor
+    { provide: HTTP_INTERCEPTORS, useClass: ApiKeyInterceptor, multi: true },
+    
     provideAnimationsAsync(), 
     providePrimeNG({
       theme: {
